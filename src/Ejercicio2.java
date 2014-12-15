d248f23878c234c2ee88001b603667d64cc893a3
zubiri <irecaldema14dw@ikzubirimanteo.com>
2014-12-15 15:26:42 +0000

    3.15

--- src/Ejercicio2.java
+++ src/Ejercicio2.java
@@ -7,23 +7,27 @@ Ordenar
 Visualizar el array después de ordenar
 */
 import java.util.ArrayList;
+import java.util.Collections;
 public class Ejercicio2 {
 	public static void main (String args[]) {
-        ArrayList<Integer> al_numeros = new ArrayList<Integer>(5);
-        //-2, 5, 1, 9, -6
-        al_numeros.add(-2); 
-		al_numeros.add(5);
-		al_numeros.add(1);
-		al_numeros.add(9);
-		al_numeros.add(-6);
-		
-		for (int i=0; i<al_numeros.size();i++){
-    	    System.out.print(" "+al_numeros.get(i)+" ");
-    	}
-    	
-    	al_numeros.Sort();
-    	for (int i=0; i<al_numeros.size();i++){
-    	    System.out.print(" "+al_numeros.get(i)+" ");
-    	}
+	        ArrayList<Integer> al_numeros = new ArrayList<Integer>(5);
+	        //-2, 5, 1, 9, -6
+	        al_numeros.add(-2); 
+			al_numeros.add(5);
+			al_numeros.add(1);
+			al_numeros.add(9);
+			al_numeros.add(-6);
+			
+			for (int i=0; i<al_numeros.size();i++){
+	    	    System.out.print(" "+al_numeros.get(i)+" ");
+	    	}
+	    	System.out.println();
+	    	System.out.println("version ordenada: ");
+	    	
+	    	Collections.sort(al_numeros);
+	    	for (int i=0; i<al_numeros.size();i++){
+	    	    System.out.print(" "+al_numeros.get(i)+" ");
+	    	}
+	    	System.out.println();
 	}
 }		    
