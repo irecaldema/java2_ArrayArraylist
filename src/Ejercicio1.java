f5e76eff3e4d9bb275ecf9862a058029671efc4a
zubiri <irecaldema14dw@ikzubirimanteo.com>
2014-12-15 13:12:04 +0000

    3.15

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -8,8 +8,34 @@ Visualizar el array después de ordenar
 */
 import java.util.ArrayList;
 public class Ejercicio1 {
-	public static void main (String args[]) throws IOException 	{
-	  
+	public static void main (String args[]) {
 
+    	int[] numeros = {-2, 5, 1, 9, -6};
+    	int[] oredenados = new int[5];
+    	System.out.println();
+    	for (int i=0; i<numeros.length;i++){
+    	    System.out.print(" "+numeros[i]+" ");
+    	}
+    	System.out.println();
+    	int comodin = -100;
+    	int pos=0;
+    	for (int n=0; n<oredenados.length;n++){
+        	for (int i=0; i<numeros.length;i++){
+        	    if (numeros[i]>=comodin){
+        	        comodin=numeros[i];
+        	        pos=i;
+        	    }  
+        	}
+    	    oredenados[n]=comodin;
+    	    numeros[pos]=-100;
+    	    comodin=-100;
+    	}  
+    	for (int i=0; i<oredenados.length;i++){
+    	    System.out.print(" "+oredenados[i]+" ");
+    	}/*
+    	System.out.println();
+    	for (int i=0; i<numeros.length;i++){
+    	    System.out.print(" "+numeros[i]+" ");
+    	}*/
 	}
 }		    
