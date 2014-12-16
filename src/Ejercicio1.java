51843521ceeebea0296acf9bc192890ece95be28
zubiri <irecaldema14dw@ikzubirimanteo.com>
2014-12-16 07:43:23 +0000

    3.15

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -5,36 +5,37 @@ Array de 5 elementos
 Visualizar el array antes de ordenar
 Ordenar
 Visualizar el array después de ordenar
-*/
+*/import java.util.Arrays;
 public class Ejercicio1 {
 	public static void main (String args[]) {
-
     	int[] numeros = {-2, 5, 1, 9, -6};
-    	int[] oredenados = new int[5];
-    	System.out.println();
+    	int[] ordenados = new int[5];
+    	System.out.println("Contenido del array");
     	for (int i=0; i<numeros.length;i++){
     	    System.out.print(" "+numeros[i]+" ");
     	}
-    	System.out.println();
+    	System.out.println();/*
     	int comodin = -100;
     	int pos=0;
-    	for (int n=0; n<oredenados.length;n++){
+    	for (int n=0; n<ordenados.length;n++){
         	for (int i=0; i<numeros.length;i++){
         	    if (numeros[i]>=comodin){
         	        comodin=numeros[i];
         	        pos=i;
         	    }  
         	}
-    	    oredenados[n]=comodin;
+    	    ordenados[n]=comodin;
     	    numeros[pos]=-100;
     	    comodin=-100;
     	}  
-    	for (int i=0; i<oredenados.length;i++){
-    	    System.out.print(" "+oredenados[i]+" ");
-    	}/*
-    	System.out.println();
+    	for (int i=0; i<ordenados.length;i++){
+    	    System.out.print(" "+ordenados[i]+" ");
+    	}*/
+    	System.out.println("Array ordenado:");
+    	Arrays.sort(numeros);    	
     	for (int i=0; i<numeros.length;i++){
     	    System.out.print(" "+numeros[i]+" ");
-    	}*/
+    	}
+    	System.out.println();    	
 	}
 }		    
